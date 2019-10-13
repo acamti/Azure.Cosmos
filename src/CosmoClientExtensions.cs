146305672ea639130df4217527aa130500421501
Manuel Mayer <manuel.mayer@acamti.ca>
2019-10-13 08:11:51 -0400

    Project setup

---
+++ src/CosmoClientExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AcamTI.Azure.Cosmos.DependencyInjection
+{
+    public static class CosmoClientDependencyInjection
+    {
+        public static IServiceCollection AddCosmosDb(this IServiceCollection services, string endpoint, string key)
+        {
+            var client = new CosmosClient(endpoint, key);
+
+            services.AddSingleton(client);
+
+            return services;
+        }
+    }
+}
