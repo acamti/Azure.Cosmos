48976b78d02124e9caf5512593518493e7b791c4
Manuel Mayer <manuel.mayer@acamti.ca>
2019-10-26 09:40:33 -0400

    Cosmo Client now initialized with connection string

--- src/CosmoClientExtensions.cs
+++ src/CosmoClientExtensions.cs
@@ -5,9 +5,18 @@ namespace AcamTI.Azure.Cosmos.DependencyInjection
 {
     public static class CosmoClientDependencyInjection
     {
-        public static IServiceCollection AddCosmosDb(this IServiceCollection services, string endpoint, string key)
+        public static IServiceCollection AddCosmosDb(this IServiceCollection services, string endpoint, string key, CosmosClientOptions clientOptions = null)
         {
-            var client = new CosmosClient(endpoint, key);
+            var client = new CosmosClient(endpoint, key, clientOptions);
+
+            services.AddSingleton(client);
+
+            return services;
+        }
+
+        public static IServiceCollection AddCosmosDb(this IServiceCollection services, string connString, CosmosClientOptions clientOptions = null)
+        {
+            var client = new CosmosClient(connString, clientOptions);
 
             services.AddSingleton(client);
 
