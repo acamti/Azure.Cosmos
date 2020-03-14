2934373fb6904e9fcc142c9bbeb58912448d35ee
Manuel Mayer <manuel.mayer@acamti.ca>
2020-03-14 19:05:37 -0400

    added cosmosProxy

---
+++ src/CosmosProxy/CosmosProxyConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace Acamti.Azure.Cosmos.CosmosProxy
+{
+    public class CosmosProxyConfiguration
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseId { get; set; }
+        public string ContainerId { get; set; }
+    }
+}
